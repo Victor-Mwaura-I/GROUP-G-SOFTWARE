01f91f83dbfa6ecf659c6824402408d7df93f533
BSCLMR147323 <157101400+BSCLMR147323@users.noreply.github.com>
2024-03-11 14:31:21 +0300

    Add files via upload

--- SCHOOL VOTING SYSTEM/main.py
+++ SCHOOL VOTING SYSTEM/main.py
@@ -6,5 +6,16 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/register")
+def register():
+  form=RegistrationForm()
+  return render_template("registration.html", title=Register, form=form)
+
+
+@app.route("/register")
+def register():
+  form=LoginForm()
+  return render_template("login.html", title=Login, form=form)
+
 if __name__ == "__main__":
    app.run()
